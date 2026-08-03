4b4f11a5ef1725fbc572305c514a8df6f3efee3a
vaibhavrajsinghSys <vaibhav.raj@sysquare.com>
2026-08-03 12:10:12 +0530

    feature/new_UI_from_resume - changes in config.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+// Ignore missing type declarations for side-effect CSS import
+// @ts-ignore
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
